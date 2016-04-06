1d97fa0a712c22bff9676c44341c620c345dbf9f
Nathan <Nathan.Alard@Hotmail.com>
2016-04-06 12:10:27 +0200

    Solution files

---
+++ Mp3Joiner/Mp3Joiner/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Mp3Joiner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFileOne = new System.Windows.Forms.TextBox();
+            this.txtFileTwo = new System.Windows.Forms.TextBox();
+            this.btnSearchOne = new System.Windows.Forms.Button();
+            this.btnSearchTwo = new System.Windows.Forms.Button();
+            this.btnMerge = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFileOne
+            // 
+            this.txtFileOne.Location = new System.Drawing.Point(63, 45);
+            this.txtFileOne.Name = "txtFileOne";
+            this.txtFileOne.Size = new System.Drawing.Size(1075, 31);
+            this.txtFileOne.TabIndex = 0;
+            // 
+            // txtFileTwo
+            // 
+            this.txtFileTwo.Location = new System.Drawing.Point(63, 142);
+            this.txtFileTwo.Name = "txtFileTwo";
+            this.txtFileTwo.Size = new System.Drawing.Size(1075, 31);
+            this.txtFileTwo.TabIndex = 1;
+            // 
+            // btnSearchOne
+            // 
+            this.btnSearchOne.Location = new System.Drawing.Point(1158, 45);
+            this.btnSearchOne.Name = "btnSearchOne";
+            this.btnSearchOne.Size = new System.Drawing.Size(98, 31);
+            this.btnSearchOne.TabIndex = 2;
+            this.btnSearchOne.Text = "Search";
+            this.btnSearchOne.UseVisualStyleBackColor = true;
+            this.btnSearchOne.Click += new System.EventHandler(this.btnSearchOne_Click);
+            // 
+            // btnSearchTwo
+            // 
+            this.btnSearchTwo.Location = new System.Drawing.Point(1158, 142);
+            this.btnSearchTwo.Name = "btnSearchTwo";
+            this.btnSearchTwo.Size = new System.Drawing.Size(98, 30);
+            this.btnSearchTwo.TabIndex = 3;
+            this.btnSearchTwo.Text = "Search";
+            this.btnSearchTwo.UseVisualStyleBackColor = true;
+            this.btnSearchTwo.Click += new System.EventHandler(this.btnSearchTwo_Click);
+            // 
+            // btnMerge
+            // 
+            this.btnMerge.Location = new System.Drawing.Point(474, 333);
+            this.btnMerge.Name = "btnMerge";
+            this.btnMerge.Size = new System.Drawing.Size(251, 84);
+            this.btnMerge.TabIndex = 4;
+            this.btnMerge.Text = "Merge";
+            this.btnMerge.UseVisualStyleBackColor = true;
+            this.btnMerge.Click += new System.EventHandler(this.btnMerge_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1279, 558);
+            this.Controls.Add(this.btnMerge);
+            this.Controls.Add(this.btnSearchTwo);
+            this.Controls.Add(this.btnSearchOne);
+            this.Controls.Add(this.txtFileTwo);
+            this.Controls.Add(this.txtFileOne);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFileOne;
+        private System.Windows.Forms.TextBox txtFileTwo;
+        private System.Windows.Forms.Button btnSearchOne;
+        private System.Windows.Forms.Button btnSearchTwo;
+        private System.Windows.Forms.Button btnMerge;
+    }
+}
+
---
+++ Mp3Joiner/Mp3Joiner/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using NAudio;
+using NAudio.Wave;
+using System.Diagnostics;
+
+namespace Mp3Joiner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearchOne_Click(object sender, EventArgs e)
+        {
+            string fileOneDir = string.Empty;
+            
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Multiselect = false;
+            if (openFile.ShowDialog(this) == DialogResult.OK)
+            {
+                fileOneDir = openFile.InitialDirectory.ToString() + openFile.FileName.ToString();
+            }
+
+            if (fileOneDir != "" && fileOneDir != null && fileOneDir.Length > 0)
+            {
+                txtFileOne.Text = fileOneDir;
+            }
+        }
+
+        private void btnSearchTwo_Click(object sender, EventArgs e)
+        {
+            string fileTwoDir = string.Empty;
+
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Multiselect = false;
+            if (openFile.ShowDialog(this) == DialogResult.OK)
+            {
+                fileTwoDir = openFile.InitialDirectory.ToString() + openFile.FileName.ToString();
+            }
+
+            if (fileTwoDir != "" && fileTwoDir != null && fileTwoDir.Length > 0)
+            {
+                txtFileTwo.Text = fileTwoDir;
+            }
+        }
+
+        private void btnMerge_Click(object sender, EventArgs e)
+        {
+            if (txtFileOne.Text != "" && txtFileOne.Text != null && txtFileOne.Text.Length > 0 && txtFileTwo.Text != "" && txtFileTwo.Text != null && txtFileTwo.Text.Length > 0)
+            {
+                string rootFolderPath = Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+                string folderPath = Path.Combine(rootFolderPath, "MergedMp3s");
+
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+
+                string fileLocation = Path.Combine(folderPath, "MergedMp3_" + DateTime.Now.ToShortDateString().Replace('/','_') + ".mp3");
+
+                Mp3FileReader readers = new Mp3FileReader(txtFileOne.Text);
+                WaveFormat targetFormat = new WaveFormat();
+                WaveStream convertedStreamOne = new WaveFormatConversionStream(targetFormat, readers);
+                WaveFileWriter.CreateWaveFile(Path.Combine(folderPath,"firstwav.wav"), convertedStreamOne);
+
+                readers = new Mp3FileReader(txtFileTwo.Text);
+                targetFormat = new WaveFormat();
+                WaveStream convertedStreamTwo = new WaveFormatConversionStream(targetFormat, readers);
+                WaveFileWriter.CreateWaveFile(Path.Combine(folderPath,"secondwav.wav"), convertedStreamTwo);
+
+                string[] inputFiles = new string[2];
+                Stream output = new MemoryStream();
+                inputFiles[0] = Path.Combine(folderPath, "firstwav.wav");
+                inputFiles[1] = Path.Combine(folderPath,"secondwav.wav");
+                mixWAVFiles(inputFiles, fileLocation);
+
+                MessageBox.Show("Merged!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else
+            {
+                MessageBox.Show("Please select two audio files!", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+
+        public void mixWAVFiles(string[] inputFiles, string fileLocation)
+        {
+            int count = inputFiles.GetLength(0);
+            WaveMixerStream32 mixer = new WaveMixerStream32();
+            WaveFileReader[] reader = new WaveFileReader[count];
+            WaveChannel32[] channelSteam = new WaveChannel32[count];
+            mixer.AutoStop = true;
+
+            for (int i = 0; i < count; i++)
+            {
+                reader[i] = new WaveFileReader(inputFiles[i]);
+                channelSteam[i] = new WaveChannel32(reader[i]);
+                mixer.AddInputStream(channelSteam[i]);
+            }
+            mixer.Position = 0;
+            WaveFileWriter.CreateWaveFile(fileLocation, mixer);
+            convertWAVtoMP3(fileLocation);
+        }
+
+        public void convertWAVtoMP3(string wavfile)
+        {
+            //string lameEXE = @"C:\Users\Jibran\Desktop\MP3 Merger\bin\Debug\lame.exe";
+            string lameEXE = Path.GetDirectoryName(Application.ExecutablePath) + "/lame.exe";
+            string lameArgs = "-V2";
+
+            string wavFile = wavfile;
+            string mp3File = "mixed.mp3";
+
+            Process process = new Process();
+            process.StartInfo = new ProcessStartInfo();
+            process.StartInfo.FileName = lameEXE;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.StartInfo.Arguments = string.Format(
+                "{0} {1} {2}",
+                lameArgs,
+                wavFile,
+                mp3File);
+
+            process.Start();
+            process.WaitForExit();
+
+            int exitCode = process.ExitCode;
+        }
+    }
+}
